076d1633fafb81487954266fc9e40598ef3b60c5
DungFreelancer <dungdo11051991@gmail.com>
2018-05-09 23:56:06 +0700

    + Add: ModeSelectionVC.

---
+++ LetsDoIt/ViewControllers/ModeSelectionVC.swift
@@ -0,0 +1,16 @@
+//
+//  ModeSelectionVC.swift
+//  LetsDoIt
+//
+//  Created by Dung Do on 5/9/18.
+//  Copyright © 2018 HD. All rights reserved.
+//
+
+class ModeSelectionVC: BaseVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}

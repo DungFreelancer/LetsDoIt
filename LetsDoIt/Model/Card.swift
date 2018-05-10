0bc377f942f6a2aaf69ed8cd51c6efeaa7cabab8
DungFreelancer <dungdo11051991@gmail.com>
2018-05-10 21:49:20 +0700

    - Change: refactor struct Card.

--- LetsDoIt/Model/Card.swift
+++ LetsDoIt/Model/Card.swift
@@ -9,35 +9,13 @@
 import UIKit
 
 struct Card: Codable {
-    var cardImage: String
-    var cardTitle:String
     
-    init(cardImage:String, cardTitle:String) {
-        self.cardImage = cardImage
-        self.cardTitle = cardTitle
+    var image: String
+    var title: String
+    
+    init(image: String, title: String) {
+        self.image = image
+        self.title = title
     }
+    
 }
-
-
-// struct, class, enum can confirm Codable.
-//import UIKit
-//enum Gender: Int, Codable {
-//    case male
-//    case female
-//}
-//
-//struct Person: Codable {
-//    var name: String
-//    var age: Int
-//    var sex: Gender
-//}
-//
-//var dungdo = Person(name: "Dung Do", age: 26, sex: .male)
-//var data: Data = try! JSONEncoder().encode(dungdo)
-//dungdo = try! JSONDecoder().decode(Person.self, from: data)
-//print(dungdo.name)
-//
-//var arrPerson = [dungdo, dungdo, dungdo]
-//data = try! JSONEncoder().encode(arrPerson)
-//arrPerson = try! JSONDecoder().decode([Person].self, from: data)
-//print(arrPerson[2])
